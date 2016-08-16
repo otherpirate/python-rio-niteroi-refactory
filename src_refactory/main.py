75844c28c91404f46e347e9d372e4844d2606326
otherpirate <mauro_murari@hotmail.com>
2016-08-16 15:56:51 -0300

    Refactoring Construction

--- src_refactory/main.py
+++ src_refactory/main.py
@@ -1,39 +1,11 @@
 from __future__ import absolute_import
-from src_refactory.prices import Prices
-
-def define_cimento(configuracao):
-    if configuracao['tijolo'] == '4 furos':
-        configuracao['cimento'] = 'fino'
-    elif configuracao['tijolo'] == '8 furos':
-        configuracao['cimento'] = 'medio'
-    elif configuracao['tijolo'] == '12 furos':
-        configuracao['cimento'] = 'forte'
-    else:
-        configuracao['cimento'] = 'nao existe'
+from src_refactory.construction import Construction
 
 
-def cotacao_por_m2(configuracao):
-    prices = Prices()
-    return prices.brick_for(configuracao['tijolo']) + prices.cement_for(configuracao['cimento'])
-
-
-def main(andares):
-    configuracao = {}
-
-    ## Escolhe o tijolo
-    if andares <= 3:
-        configuracao['tijolo'] = '4 furos'
-    elif andares >= 4 and andares <= 10:
-        configuracao['tijolo'] = '8 furos'
-    else:
-        configuracao['tijolo'] = '12 furos'
-
-    # Define o cimento
-    define_cimento(configuracao)
-
-    return cotacao_por_m2(configuracao)
+def calculate_price(floors):
+    return Construction(floors).price_for_each_meter
 
 
 if __name__ == '__main__':
-    print (main(andares=4))
+    print (calculate_price(andares=4))
 
--- test_refactory/test_main.py
+++ test_refactory/test_main.py
@@ -1,79 +1,31 @@
 from __future__ import absolute_import
 import unittest
 from src_refactory.prices import Prices
-from src_refactory.main import define_cimento, cotacao_por_m2, \
-    main
-
+from src_refactory.main import calculate_price
 
 
 class MainTests(unittest.TestCase):
 
     def setUp(self):
-        self.configuracao = {}
         self.prices = Prices()
 
-    def test_define_cimento_fino(self):
-        self.configuracao['tijolo'] = '4 furos'
-        self.assertNotIn('cimento', self.configuracao)
+    def test_can_calculate_small(self):
+        brick_price = self.prices.brick_for('4 furos')
+        cement_price = self.prices.cement_for('fino')
+        for floor in range(1, 4):
+            meter_price = calculate_price(floor)
+            self.assertEqual(meter_price, cement_price + brick_price)
 
-        define_cimento(self.configuracao)
-        self.assertIn('cimento', self.configuracao)
-        self.assertEqual(self.configuracao['cimento'], 'fino')
+    def test_can_calculate_medium(self):
+        brick_price = self.prices.brick_for('8 furos')
+        cement_price = self.prices.cement_for('medio')
+        for floor in range(4, 11):
+            meter_price = calculate_price(floor)
+            self.assertEqual(meter_price, cement_price + brick_price)
 
-    def test_define_cimento_medio(self):
-        self.configuracao['tijolo'] = '8 furos'
-        self.assertNotIn('cimento', self.configuracao)
-
-        define_cimento(self.configuracao)
-        self.assertIn('cimento', self.configuracao)
-        self.assertEqual(self.configuracao['cimento'], 'medio')
-
-    def test_define_cimento_forte(self):
-        self.configuracao['tijolo'] = '12 furos'
-        self.assertNotIn('cimento', self.configuracao)
-
-        define_cimento(self.configuracao)
-        self.assertIn('cimento', self.configuracao)
-        self.assertEqual(self.configuracao['cimento'], 'forte')
-
-    def test_define_cimento_nao_cadastrado(self):
-        self.configuracao['tijolo'] = '10 furos'
-        self.assertNotIn('cimento', self.configuracao)
-
-        define_cimento(self.configuracao)
-        self.assertIn('cimento', self.configuracao)
-        self.assertEqual(self.configuracao['cimento'], 'nao existe')
-
-    def test_cotar_preco(self):
-        self.configuracao['tijolo'] = '12 furos'
-        define_cimento(self.configuracao)
-        cotacao = cotacao_por_m2(self.configuracao)
-
-        preco_tijolo = self.prices.brick_for('12 furos')
-        preco_cimento = self.prices.cement_for('forte')
-
-        self.assertEqual(cotacao, preco_tijolo + preco_cimento)
-
-    def test_calcula_para_ate_tres_andares(self):
-        preco_tijolo = self.prices.brick_for('4 furos')
-        preco_cimento = self.prices.cement_for('fino')
-
-        for andar in range(1, 4):
-            cotacao = main(andares=andar)
-            self.assertEqual(cotacao, preco_cimento + preco_tijolo)
-
-    def test_calcula_de_quatro_a_dez_andares(self):
-        preco_tijolo = self.prices.brick_for('8 furos')
-        preco_cimento = self.prices.cement_for('medio')
-
-        for andar in range(4, 11):
-            cotacao = main(andares=andar)
-            self.assertEqual(cotacao, preco_cimento + preco_tijolo)
-
-    def test_calcula_mais_de_dez_andares(self):
-        preco_tijolo = self.prices.brick_for('12 furos')
-        preco_cimento = self.prices.cement_for('forte')
-
-        for andar in range(11, 21):
-            cotacao = main(andares=andar)
-            self.assertEqual(cotacao, preco_cimento + preco_tijolo)
+    def test_can_calculate_big(self):
+        brick_price = self.prices.brick_for('12 furos')
+        cement_price = self.prices.cement_for('forte')
+        for floor in range(11, 21):
+            meter_price = calculate_price(floor)
+            self.assertEqual(meter_price, cement_price + brick_price)
